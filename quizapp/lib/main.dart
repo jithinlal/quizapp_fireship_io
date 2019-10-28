8a76a5d518eaba5859cc1a6d51a31a6809dd99c5
jithinlal <jithinlal@softwareassociates.in>
2019-10-28 14:16:07 +0530

    data and flutter

---
+++ quizapp/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class UserModel {
+  String name = 'Bob';
+}
+
+var stream = Stream.fromIterable([UserModel()]);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<UserModel>.value(
+      value: stream,
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final Firestore db = Firestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    var user = Provider.of<UserModel>(context);
+
+    return Scaffold(
+      body: Center(
+        child: Text(user.name),
+      ),
+    );
+  }
+}
