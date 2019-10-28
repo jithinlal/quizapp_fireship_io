96304f7673a89b0c4a8af8102603ebcd65fe331f
jithinlal <jithinlal@softwareassociates.in>
2019-10-28 14:22:51 +0530

    security fix

--- fireship-quizapp-data/quizzes.js
+++ fireship-quizapp-data/quizzes.js
@@ -3,47 +3,41 @@ const fs = require('fs-extra');
 const yaml = require('yamljs');
 
 admin.initializeApp({
-    credential: admin.credential.cert(require('./credentials.json')),
+	credential: admin.credential.cert(require('./credentials.json'))
 });
 const db = admin.firestore();
 
 const quizzes = [
-    'angular-basics',
-    'flutter-scroll',
-    'cf-basics',
-    'cf-triggers',
-    'firebase-perf',
-    'firestore-basics',
-    'flutter-basics',
-    'flutter-containers',
-    'flutter-flex',
-    'flutter-gestures',
-    'flutter-material',
-    'flutter-scroll',
-    'js-basics',
-    'js-variables',
-    'rxjs-basics',
-    'ts-basics'
-]
+	'angular-basics',
+	'flutter-scroll',
+	'cf-basics',
+	'cf-triggers',
+	'firebase-perf',
+	'firestore-basics',
+	'flutter-basics',
+	'flutter-containers',
+	'flutter-flex',
+	'flutter-gestures',
+	'flutter-material',
+	'flutter-scroll',
+	'js-basics',
+	'js-variables',
+	'rxjs-basics',
+	'ts-basics'
+];
 
+const update = async quizId => {
+	const json = yaml.load(`quizzes/${quizId}.yaml`);
 
-const update = async(quizId) => {
+	console.log(JSON.stringify(json));
 
-    const json = yaml.load(`quizzes/${quizId}.yaml`);
+	const ref = db.collection('quizzes').doc(quizId);
 
-    console.log(JSON.stringify(json));
+	await ref.set(json, { merge: true });
 
-    const ref = db.collection('quizzes').doc(quizId);
-
-    await ref.set(json, { merge: true });
-
-    console.log('DONE');
-
-}
+	console.log('DONE');
+};
 
 for (const quiz of quizzes) {
-    update(quiz);
+	update(quiz);
 }
-
-
-
