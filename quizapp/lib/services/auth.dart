106c9150c1788bfbed0f3b94c1c304b4f5cabc81
jithinlal <jithinlal@softwareassociates.in>
2019-10-29 16:04:44 +0530

    final

--- quizapp/lib/services/auth.dart
+++ quizapp/lib/services/auth.dart
@@ -1,78 +1,16 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'dart:async';
-
-// class AuthService {
-//   final GoogleSignIn _googleSignIn = GoogleSignIn();
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//   final Firestore _db = Firestore.instance;
-
-//   Future<FirebaseUser> get getUser => _auth.currentUser();
-
-//   Stream<FirebaseUser> get user => _auth.onAuthStateChanged;
-
-//   Future<FirebaseUser> googleSignIn() async {
-//     try {
-//       GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
-//       GoogleSignInAuthentication googleAuth =
-//           await googleSignInAccount.authentication;
-
-//       final AuthCredential credential = GoogleAuthProvider.getCredential(
-//         accessToken: googleAuth.accessToken,
-//         idToken: googleAuth.idToken,
-//       );
-
-//       final FirebaseUser user =
-//           (await _auth.signInWithCredential(credential)).user;
-//       updateUserData(user);
-
-//       return user;
-//     } catch (error) {
-//       print(error);
-//       return null;
-//     }
-//   }
-
-//   Future<FirebaseUser> anonLogin() async {
-//     FirebaseUser user = (await _auth.signInAnonymously()).user;
-//     updateUserData(user);
-//     return user;
-//   }
-
-//   Future<void> updateUserData(FirebaseUser user) {
-//     DocumentReference reportRef = _db.collection('reports').document(user.uid);
-
-//     return reportRef.setData({'uid': user.uid, 'lastActivity': DateTime.now()},
-//         merge: true);
-//   }
-
-//   Future<void> signOut() {
-//     return _auth.signOut();
-//   }
-// }
-
-// // keytool -list -v -alias androiddebugkey -keystore %USERPROFILE%\.android\debug.keystore
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 
 class AuthService {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final Firestore _db = Firestore.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<FirebaseUser> get getUser => _auth.currentUser();
+
   Stream<FirebaseUser> get user => _auth.onAuthStateChanged;
-
-  Future<FirebaseUser> anonLogin() async {
-    FirebaseUser user = (await _auth.signInAnonymously()).user;
-
-    updateUserData(user);
-    return user;
-  }
 
   Future<FirebaseUser> googleSignIn() async {
     try {
@@ -87,6 +25,7 @@ class AuthService {
 
       FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
       updateUserData(user);
+
       return user;
     } catch (error) {
       print(error);
@@ -94,19 +33,22 @@ class AuthService {
     }
   }
 
+  Future<FirebaseUser> anonLogin() async {
+    FirebaseUser user = (await _auth.signInAnonymously()).user;
+    updateUserData(user);
+    return user;
+  }
+
   Future<void> updateUserData(FirebaseUser user) {
     DocumentReference reportRef = _db.collection('reports').document(user.uid);
 
-    return reportRef.setData(
-      {
-        'uid': user.uid,
-        'lastActivity': DateTime.now(),
-      },
-      merge: true,
-    );
+    return reportRef.setData({'uid': user.uid, 'lastActivity': DateTime.now()},
+        merge: true);
   }
 
   Future<void> signOut() {
     return _auth.signOut();
   }
 }
+
+// keytool -list -v -alias androiddebugkey -keystore %USERPROFILE%\.android\debug.keystore
